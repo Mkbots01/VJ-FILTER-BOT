a2b1b23e3ba00fe2543bc9203c74cf156680a3cd
Mkbots01 <kondapallishiva144@gmail.com>
2025-01-01 21:21:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+E3QrOnDeppplNDY1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MKStreamingHub')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/MK_StreamingHub) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MKStreamingHub) # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mk_streaminghub')
 
 # True Or False
